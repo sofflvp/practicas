d9a0952ba0ddc7206f02157e5cc34bfad3ca2f76
MarianRod <marianorod.msk@gmail.com>
2017-06-02 21:44:02 -0300

    pract2

---
+++ pract2/pract2.py
@@ -0,0 +1,20 @@
+v = "hola "
+print(v*5)
+car = raw_input ("ingrese un caracter: ")
+if(car == "A"):
+	print("65")
+elif(car == "a"):
+	print("97")
+elif(int(car) == 0):
+	print("48")
+else:
+	print("No es A, a, 0")
+hola = "s"
+print(hola)
+cacho = input("ingrese numero: ")
+if cacho > 0:
+	print "positivo"
+elif (cacho < 0):
+	print "negativo"
+else:
+	print "es cero"
